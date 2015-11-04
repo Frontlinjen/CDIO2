2f3f31af261d863072461c68b2caef63ec4f2534
MikkelLeth <MikkelLeth@mikkels-mbp-2.home>
2015-11-04 12:34:04 +0100

    changed names to english

--- src/Test/TestDicePair.java
+++ src/Test/TestDicePair.java
@@ -15,20 +15,20 @@ public class TestDicePair {
 	public void testRollDice() {
 		BaseDice dice = new DicePair();
 		double[] test = new double[11];
-		double[] sandsynlighed = {(1.0d/36*100),(1.0d/18*100),(1.0d/12*100),(1.0d/9*100),(5.0d/36*100),(1.0d/6*100),(5.0d/36*100),(1.0d/9*100),(1.0d/12*100),(1.0d/18*100),(1.0d/36*100)};
-		double[] slag = new double [11];
+		double[] probability = {(1.0d/36*100),(1.0d/18*100),(1.0d/12*100),(1.0d/9*100),(5.0d/36*100),(1.0d/6*100),(5.0d/36*100),(1.0d/9*100),(1.0d/12*100),(1.0d/18*100),(1.0d/36*100)};
+		double[] roll = new double [11];
 		for(int i = 0; i <= 5000; i++){
 			dice.rollDice();
 			test[dice.result()-2]++;
 		}
 		for(int j = 0; j < test.length; j++) {
 			double percent = ((test[j] / 5000)*100);
-			double afvigelse = Math.abs(percent - sandsynlighed[j]);
+			double deviation = Math.abs(percent - probability[j]);
 			double retur = 0;
 			System.out.println("Hvis du slår " + (j+2) + " så er er sandsynligheden: " + percent + " %");
-			System.out.println("Afvigelsen for den statistiske sandsynlighed for at slå " + (j+2) + " bliver: " + afvigelse);
-			retur += afvigelse;
-			assertTrue(2.5 > afvigelse);
+			System.out.println("Afvigelsen for den statistiske sandsynlighed for at slå " + (j+2) + " bliver: " + deviation);
+			retur += deviation;
+			assertTrue(2.5 > deviation);
 		}
 	}
 
